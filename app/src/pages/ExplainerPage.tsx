e578fd8ce298b035cd5f9ed577126c44d7b42432
hectorjak <hectorheltj@gmail.com>
2025-05-16 18:56:11 +0200

    t

--- app/src/pages/ExplainerPage.tsx
+++ app/src/pages/ExplainerPage.tsx
@@ -1,5 +1,16 @@
 const ExplainerPage: React.FC = () => {
-  return <></>;
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-4">Notebook Explainer</h1>
+      <iframe
+        src="https://nbviewer.org/github/Andreas-Holm-2/02467-Project-assignment/raw/main/explainer.ipynb?v=2"
+        width="100%"
+        height="1000px"
+        frameBorder="0"
+        title="Jupyter Notebook"
+      />
+    </div>
+  );
 };
 
 export default ExplainerPage;
