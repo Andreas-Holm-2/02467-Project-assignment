74cb1af4c495a95d9ae3e0bf13da04870c3b096c
“Andreas-holm-2” <s234838@dtu.dk>
2025-05-16 16:27:18 +0200

    Merge branch 'main' of https://github.com/Andreas-Holm-2/02467-Project-assignment

---
+++ app/src/components/WordCloud.tsx
@@ -0,0 +1,24 @@
+import { Typography } from "@mui/material";
+import { PaperWrapper } from "./PaperWrapper";
+
+import Pop1 from "../assets/pop1.png";
+import Pop2 from "../assets/pop2.png";
+import Pop3 from "../assets/pop3.png";
+import Pop4 from "../assets/Pop4.png";
+import Pop5 from "../assets/Pop5.png";
+
+import Rap1 from "../assets/Rap1.png";
+import Rap2 from "../assets/Rap2.png";
+import Rap3 from "../assets/Rap3.png";
+import Rap4 from "../assets/Rap4.png";
+import Rap5 from "../assets/Rap5.png";
+
+const WordCloud = () => {
+  return (
+    <PaperWrapper>
+      <Typography>Text Analysis</Typography>
+    </PaperWrapper>
+  );
+};
+
+export default WordCloud;
